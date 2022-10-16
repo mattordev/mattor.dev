85bc3df8474f27cfdd0e2b6ad28489a3a33097d6
mr232432 <mr232432@falmouth.ac.uk>
2022-10-16 05:03:36 +0100

    fix: fixed index CSS from being loaded on every page.
    load-master.js was referenceing index.css in a function. Extracted it and moved it to index.js
    
    other minor changes, removed contact me from it aswell.

--- js/load-master.js
+++ js/load-master.js
@@ -3,28 +3,6 @@ let masterpage = fetch("/templates/master.json")
   .then((response) => response.json())
   .then((json) => parsePageInfo(json));
 
-// generates a html main from json
-function generateMain(data) {
-  element = document.getElementById("head");
-  element.innerHTML += `
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>MATTORDEV</title>
-        <link rel="stylesheet" href="/css/index.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href=${data.favicon}>                  
-    `;
-
-  element = document.getElementById("title");
-  element.innerHTML = data.title + element.innerHTML;
-}
-
 // generates a html header from json
 function generateHeader(data) {
   // Gets the header ID from html files
@@ -88,11 +66,6 @@ function generateFooter(data) {
         <div class="footer__container">
           <div class="footer__links">
             <div class="footer__link--wrapper">
-              <div class="footer__link--items">
-                <h2>Contact Me</h2>
-                <a href="/contact-me.html">Contact</a>
-                <a href="/">Something isn't working right</a>
-              </div>
             </div>
             <div class="footer__link--wrapper"></div>
           </div>
@@ -113,7 +86,6 @@ function generateFooter(data) {
 
 // Parse the header information into JS
 async function parsePageInfo(json) {
-  generateMain(json.Main);
   generateHeader(json.Header);
   generateFooter(json.Footer);
 }
