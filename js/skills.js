2e82e12ede9ced7eda6789e4e26e6f12f4241b28
mr232432 <mr232432@falmouth.ac.uk>
2022-11-11 15:01:59 +0000

    feat: skills page icons

--- js/skills.js
+++ js/skills.js
@@ -22,7 +22,8 @@ function generateMain(data) {
         <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">                    
+        <link rel="manifest" href="/site.webmanifest">        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">            
     `;
 
   element = document.getElementById("title");
