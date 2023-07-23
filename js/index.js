6f9e4952cf6f8c6414beea1e76e78569c4ca389a
Hayley Davies <cd230099@falmouth.ac.uk>
2023-07-23 23:01:31 +0100

    fix: scaling should now work reponsively

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
 // Gets all the data corrosponding to the master-page
 let indexpage = fetch("/templates/master.json")
-  .then((response) => response.json())
-  .then((json) => generateMain(json));
+	.then((response) => response.json())
+	.then((json) => generateMain(json));
 
 // generates a html main from json
 function generateMain(data) {
-  element = document.getElementById("head");
-  element.innerHTML += `
+	element = document.getElementById("head");
+	element.innerHTML += `
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>MATTORDEV</title>
-        <link rel="stylesheet" href="/css/index.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
@@ -25,6 +24,6 @@ function generateMain(data) {
         <link rel="manifest" href="/site.webmanifest">                   
     `;
 
-  element = document.getElementById("title");
-  element.innerHTML = data.Main.title + element.innerHTML;
+	element = document.getElementById("title");
+	element.innerHTML = data.Main.title + element.innerHTML;
 }
