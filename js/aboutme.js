489708c4040ba9ac105ef77dd6ff5467d1124923
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-07-23 22:08:20 +0100

    BREAKING CHANGE: Started site V2
    everything is changing

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,17 +1,17 @@
 // Gets all the data corrosponding to the master-page
 let indexpage = fetch("/templates/master.json")
-  .then((response) => response.json())
-  .then((json) => generateMain(json));
+	.then((response) => response.json())
+	.then((json) => generateMain(json));
 
 // generates a html main from json
 function generateMain(data) {
-  element = document.getElementById("head");
-  element.innerHTML += `
+	element = document.getElementById("head");
+	element.innerHTML += `
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>MATTORDEV</title>
-        <link rel="stylesheet" href="/css/aboutme.css" />
+        <link rel="stylesheet" href="/css/about-me.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
@@ -19,12 +19,12 @@ function generateMain(data) {
           rel="stylesheet"
         />
         <link rel="icon" href=${data.Main.favicon}>
-        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">                  
     `;
 
-  element = document.getElementById("title");
-  element.innerHTML = data.Main.title + element.innerHTML;
+	element = document.getElementById("title");
+	element.innerHTML = data.Main.title + element.innerHTML;
 }
